9f0ba70a57104d3b912c1497ed2899dbb10a3af1
melvinlee <melvinlee@hotmail.com>
2018-06-22 11:43:13 +0800

    feat: added microservices backend app.

---
+++ samples/microservices/backend/server.js
@@ -0,0 +1,20 @@
+const Koa = require('koa');
+const logger = require('koa-logger');
+const router = require('koa-router')();
+const app = module.exports = new Koa();
+
+const PORT = process.env.PORT || 3000
+const SERVICE_NAME = process.env.SERVICE_NAME || 'foo'
+const VERSION = process.env.VERSION || 'v1'
+
+// middleware
+app.use(logger());
+
+// route definitions
+router.get('/',  async (ctx) => { 
+    ctx.body = `backend-${SERVICE_NAME}:${VERSION}`;
+});
+
+app.use(router.routes());
+  
+if (!module.parent) app.listen(PORT);
