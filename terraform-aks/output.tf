43ac508feb558633813ad8cdbeb3361a0ffc6034
melvinlee <melvinlee@hotmail.com>
2019-08-31 16:38:47 +0800

    feat: Suppressing kube_config_raw in CLI output.

--- terraform-aks/output.tf
+++ terraform-aks/output.tf
@@ -1,12 +1,10 @@
 ##################################################################################
 # OUTPUTS
 ##################################################################################
-output "host" {
-  value = "${azurerm_kubernetes_cluster.main.kube_config.0.host}"
-}
 
 output "kube_config" {
   value = "${azurerm_kubernetes_cluster.main.kube_config_raw}"
+  sensitive   = true
 }
 
 output "aks_dashboard" {
